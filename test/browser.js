02776d838089e412ee51a6c8310f75cec15a0f88
WXiaoming <klamtine@gmail.com>
2016-10-17 11:23:41 +0800

    initial commit

---
+++ test/browser.js
@@ -0,0 +1,24 @@
+import mocha;
+import { assert } from 'chai';
+
+mocha.setup('bdd');
+
+beforeEach(_ => {
+
+});
+
+afterEach(_ => {});
+
+
+describe('send requests', _ => {
+  it('should send get request', _ => {});
+
+  it('should send post request', _ => {});
+
+  it('should send patch request', _ => {});
+
+  it('should send delete request', _ => {});
+
+  it('should send put request', _ => {});
+});
+
